d731483b4240087aff08688be909324c1bffeda0
pula39 <programmed_life@naver.com>
2020-05-04 17:10:19 +0900

    코드의 주석 추가, 불필요 함수 제거, set_finite_state -> set_final_state

--- classes.py
+++ classes.py
@@ -7,15 +7,16 @@ class Dfa():
         self.rules = defaultdict(dict)
         self.type = type
 
-    def set_states(self, state_list):
-        self.states = state_list
-
-    def set_finite_states(self, finite_state_list):
+    # DFA의 Final states를 작성한다.
+    def set_final_states(self, finite_state_list):
         self.finite_states = finite_state_list
 
+    # DFA의 Transition rule을 작성한다.
     def add_rule(self, from_state, to_state, transit_literal_list):
         self.rules[from_state][transit_literal_list] = to_state
 
+    # start_pos 부터 code를 봐서, Parse 될 수 있는 token이 있는지 시도해본다.
+    # Parse 된 token의 type, value, 파싱한 범위를 반환한다.
     def try_accept(self, code, start_pos):
         accepted = self.accept_this(0, code[start_pos:], 0)
 
@@ -27,7 +28,9 @@ class Dfa():
             print(code[start_pos:end_pos])
             return (self.type, code[start_pos:end_pos], end_pos)
 
+    # 제시된 literal에 대해, accept 하는지 재귀로 확인한다.
     def accept_this(self, current_state, code, current_pos):
+        # 현재 판단되는 literal을 가져온다.
         current_literal = code[current_pos:current_pos+1]
 
         literal_left = current_literal is not ''
@@ -66,22 +69,15 @@ class TokenScanner():
         self.parsed_token = []
         self.dfa_list = []
 
-    def parse_next(self):
-        parsed = self.get_current_token()
-
-        if parsed is None:
-            print("Parse Failed")
-            return False
-
-        self.parsed_token.append(parsed)
-
     def add_dfa(self, dfa):
         self.dfa_list.append(dfa)
 
+    # parsing이 성공적으로 끝난 상태인지 조회한다.
     def parse_end(self):
         return self.start_pos == len(self.code)
 
     def parse_token(self):
+        # 모든 dfa 중에 parse 성공하는 dfa를 보고, 있으면 return 한다.
         for dfa in self.dfa_list:
             ret = dfa.try_accept(self.code, self.start_pos)
 
@@ -91,6 +87,7 @@ class TokenScanner():
             ret_type, ret_value, end_pos = ret
 
             print(f"parse success. {ret}. start_pos adjusted to {end_pos}")
+            # 한 Token Parse 에 성공했으므로, start_pos를 end_pos로 해준다.
             self.start_pos = end_pos
             return (ret_type, ret_value)
 
--- main.py
+++ main.py
@@ -2,26 +2,31 @@ import sys
 from classes import *
 import pprint
 
+# system keword 에 대한 dfa를 자동생성한다.
 def make_system_dfa(name, keyword):
     digit = "1234567890"
     char = "qwertyuiopasdfghjklzxcvbnm"
 
     length = len(keyword)
     dfa = Dfa(name)
-    dfa.set_finite_states([length])
+    dfa.set_final_states([length])
+
     for i in range(0, length):
         dfa.add_rule(i,i+1, keyword[i])
 
+    # 시스템 키워드가 나오고 identifier처럼 될 수 있는 글자가 나오면 안된다.
     dfa.add_rule(length,length+1, digit + char)
 
     return dfa
 
+# 한 글자를 판단하기 위한 dfa를 작성한다.
 def make_single_dfa(name, char):
     dfa = Dfa(name)
-    dfa.set_finite_states([1])
+    dfa.set_final_states([1])
     dfa.add_rule(0, 1, char)
     return dfa
 
+# dfa 작성 코드가 너무 길어서 별도의 함수로 분리.
 def set_dfa(token_scanner):
     digit = "1234567890"
     char = "qwertyuiopasdfghjklzxcvbnm"
@@ -34,7 +39,7 @@ def set_dfa(token_scanner):
 
     # arthimatic
     arth_dfa = Dfa("arthimatic")
-    arth_dfa.set_finite_states([1, 2, 11])
+    arth_dfa.set_final_states([1, 2, 11])
     arth_dfa.add_rule(0, 1, "<>")
     arth_dfa.add_rule(1, 2, "=")
     arth_dfa.add_rule(2, 3, "=<>")
@@ -42,7 +47,7 @@ def set_dfa(token_scanner):
     arth_dfa.add_rule(0, 10, "!")
     arth_dfa.add_rule(0, 11, "=")
     arth_dfa.add_rule(10, 11, "=")
-    arth_dfa.add_rule(11, 12, "!=")
+    arth_dfa.add_rule(11, 12, "!")
 
     token_scanner.add_dfa(arth_dfa)
 
@@ -61,14 +66,14 @@ def set_dfa(token_scanner):
     token_scanner.add_dfa(make_single_dfa("SEMICOLON", ";"))
 
     iden_dfa = Dfa("identifier")
-    iden_dfa.set_finite_states([1])
+    iden_dfa.set_final_states([1])
     iden_dfa.add_rule(0, 1, char)
     iden_dfa.add_rule(1, 1, digit)
     iden_dfa.add_rule(1, 1, char)
     token_scanner.add_dfa(iden_dfa)
 
     iden_dfa = Dfa("digits")
-    iden_dfa.set_finite_states([1])
+    iden_dfa.set_final_states([1])
     iden_dfa.add_rule(0, 1, digit)
     iden_dfa.add_rule(1, 1, digit)
     iden_dfa.add_rule(0, 2, "-")
@@ -77,7 +82,7 @@ def set_dfa(token_scanner):
     token_scanner.add_dfa(iden_dfa)
 
     iden_dfa = Dfa("float")
-    iden_dfa.set_finite_states([4])
+    iden_dfa.set_final_states([4])
     iden_dfa.add_rule(0, 1, "-")
     iden_dfa.add_rule(1, 2, digit)
     iden_dfa.add_rule(0, 2, digit)
@@ -87,14 +92,14 @@ def set_dfa(token_scanner):
 
     blank = "\n\r "
     blank_dfa = Dfa("blank")
-    blank_dfa.set_finite_states([1])
+    blank_dfa.set_final_states([1])
     blank_dfa.add_rule(0, 1, blank)
     blank_dfa.add_rule(1, 1, blank)
     token_scanner.add_dfa(blank_dfa)
 
     quote = '"'
     blank_dfa = Dfa("quote")
-    blank_dfa.set_finite_states([2])
+    blank_dfa.set_final_states([2])
     blank_dfa.add_rule(0, 1, quote)
     blank_dfa.add_rule(1, 1, digit + char)
     blank_dfa.add_rule(1, 2, quote)
@@ -106,15 +111,21 @@ def main(file_path):
 
     print(literal_list)
 
+    # 읽어온 Source Code를 Parsing 하기 위해 Token Scanner에 전달합니다.
     token_scanner = TokenScanner(literal_list)
+
+    # Token을 인식하기 위한 dfa를 생성해 token_scanner에 전달해준다.
     set_dfa(token_scanner)
 
+    # Parse 된 Token을 저장하기 위한 list를 생성한다.
     token_list = []
 
     while True:
+        # Token 한개를 Parsing 해본다.
         ret = token_scanner.parse_token()
+        # ret이 None이다 -> 파싱 실패 또는 파싱 종료.
         if ret is None:
-            print(f"여기까지! {token_scanner.parse_end()}")
+            print(f"Parsing이 끝났습니다. Parsing이 성공적으로 끝났나요?{token_scanner.parse_end()}")
             pprint.pprint(token_list)
             break;
 
