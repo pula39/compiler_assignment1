c3f372cf47d5998d2c1c622662b6689d6d9265c5
sy10kr <alswnsghd15@naver.com>
2020-05-08 17:31:24 +0900

    set dfat

--- main.py
+++ main.py
@@ -26,82 +26,192 @@ def make_single_dfa(name, char):
 
 # dfa 작성 코드가 너무 길어서 별도의 함수로 분리.
 def set_dfa(token_scanner):
+    nz = "123456789"
     digit = "1234567890"
-    char = "qwertyuiopasdfghjklzxcvbnm"
+    char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    # system keword
-    token_scanner.add_dfa(make_system_dfa("integer", "int"))
-    token_scanner.add_dfa(make_system_dfa("float", "float"))
-    token_scanner.add_dfa(make_system_dfa("if", "if"))
-    token_scanner.add_dfa(make_system_dfa("else", "else"))
+    # system keword (Keyword stmt)
+    stmt_dfa = Dfa("Keyword stmt")
+    stmt_dfa.set_final_states([2, 6, 11, 14, 20])
+    stmt_dfa.add_rule(0, 1, "i")
+    stmt_dfa.add_rule(1, 2, "f")
 
-    # arthimatic
-    arth_dfa = Dfa("compare")
-    arth_dfa.set_final_states([1, 2, 11])
-    arth_dfa.add_rule(0, 1, "<>")
-    arth_dfa.add_rule(1, 2, "=")
-    arth_dfa.add_rule(2, 3, "=<>")
+    stmt_dfa.add_rule(0, 3, "e")
+    stmt_dfa.add_rule(3, 4, "l")
+    stmt_dfa.add_rule(4, 5, "s")
+    stmt_dfa.add_rule(5, 6, "e")
 
-    arth_dfa.add_rule(0, 10, "!")
-    arth_dfa.add_rule(0, 11, "=")
-    arth_dfa.add_rule(10, 11, "=")
-    arth_dfa.add_rule(11, 12, "!")
+    stmt_dfa.add_rule(0, 7, "w")
+    stmt_dfa.add_rule(7, 8, "h")
+    stmt_dfa.add_rule(8, 9, "i")
+    stmt_dfa.add_rule(9, 10, "l")
+    stmt_dfa.add_rule(10, 11, "e")
 
+    stmt_dfa.add_rule(0, 12, "f")
+    stmt_dfa.add_rule(12, 13, "o")
+    stmt_dfa.add_rule(13, 14, "r")
+
+    stmt_dfa.add_rule(0, 15, "r")
+    stmt_dfa.add_rule(15, 16, "e")
+    stmt_dfa.add_rule(16, 17, "t")
+    stmt_dfa.add_rule(17, 18, "u")
+    stmt_dfa.add_rule(18, 19, "r")
+    stmt_dfa.add_rule(19, 20, "n")
+    token_scanner.add_dfa(stmt_dfa)
+
+
+    # system keword (Vtype)
+    vtype_dfa = Dfa("Vtype")
+    vtype_dfa.set_final_states([3, 7, 11, 14, 19])
+    vtype_dfa.add_rule(0, 1, "i")
+    vtype_dfa.add_rule(1, 2, "n")
+    vtype_dfa.add_rule(2, 3, "t")
+
+    vtype_dfa.add_rule(0, 4, "c")
+    vtype_dfa.add_rule(4, 5, "h")
+    vtype_dfa.add_rule(5, 6, "a")
+    vtype_dfa.add_rule(6, 7, "r")
+
+    vtype_dfa.add_rule(0, 8, "b")
+    vtype_dfa.add_rule(9, 9, "o")
+    vtype_dfa.add_rule(9, 10, "o")
+    vtype_dfa.add_rule(10, 11, "l")
+    vtype_dfa.add_rule(11, 12, "e")
+    vtype_dfa.add_rule(12, 13, "a")
+    vtype_dfa.add_rule(13, 14, "n")
+
+    vtype_dfa.add_rule(0, 15, "f")
+    vtype_dfa.add_rule(15, 16, "l")
+    vtype_dfa.add_rule(16, 17, "o")
+    vtype_dfa.add_rule(17, 18, "a")
+    vtype_dfa.add_rule(18, 19, "t")
+    token_scanner.add_dfa(vtype_dfa)
+
+
+    # arthimatic operators
+    arth_dfa = Dfa("Arthimatic operator")
+    arth_dfa.set_final_states([1])
+    arth_dfa.add_rule(0, 1, "-")
+    arth_dfa.add_rule(0, 1, "+")
+    arth_dfa.add_rule(0, 1, "*")
+    arth_dfa.add_rule(0, 1, "/")
     token_scanner.add_dfa(arth_dfa)
 
-    # arithmatic with one token
-    token_scanner.add_dfa(make_single_dfa("arith", "+"))
-    token_scanner.add_dfa(make_single_dfa("arith", "-"))
-    token_scanner.add_dfa(make_single_dfa("arith", "/"))
-    token_scanner.add_dfa(make_single_dfa("arith", "%"))
+    # bitwise operators
+    bit_dfa = Dfa("Bitwise operator")
+    bit_dfa.set_final_states([2, 4, 5, 6])
+    bit_dfa.add_rule(0, 1, "<")
+    bit_dfa.add_rule(1, 2, "<")
+    bit_dfa.add_rule(0, 3, ">")
+    bit_dfa.add_rule(3, 4, ">")
+    bit_dfa.add_rule(0, 5, "&")
+    bit_dfa.add_rule(0, 6, "|")
+    token_scanner.add_dfa(bit_dfa)
 
-    # 괄호들
+    # comparison operators
+    comp_dfa = Dfa("Comparison operator")
+    comp_dfa.set_final_states([2, 4, 6, 8])
+    comp_dfa.add_rule(0, 1, "<")
+    comp_dfa.add_rule(1, 2, "=")
+    comp_dfa.add_rule(0, 3, ">")
+    comp_dfa.add_rule(3, 4, "=")
+    comp_dfa.add_rule(0, 5, "=")
+    comp_dfa.add_rule(5, 6, "=")
+    comp_dfa.add_rule(0, 7, "!")
+    comp_dfa.add_rule(7, 8, "=")
+    token_scanner.add_dfa(comp_dfa)
+
+    # whites space
+    ws_dfa = Dfa("WHITE SPACE")
+    ws_dfa.set_final_states([1])
+    ws_dfa.add_rule(0, 1, "\t")
+    ws_dfa.add_rule(0, 1, "\n")
+    ws_dfa.add_rule(0, 1, " ")
+    ws_dfa.add_rule(1, 1, "\t")
+    ws_dfa.add_rule(1, 1, "\n")
+    ws_dfa.add_rule(1, 1, " ")
+    token_scanner.add_dfa(ws_dfa)
+
+    #assign
+    token_scanner.add_dfa(make_single_dfa("Assign", "="))
+
+    #semicolon
+    semi_dfa = Dfa("Semicolon")
+    semi_dfa.set_final_states([1])
+    semi_dfa.add_rule(0, 1, ";")
+    token_scanner.add_dfa(semi_dfa)
+
+    # brackets
     token_scanner.add_dfa(make_single_dfa("LPAREN", "("))
     token_scanner.add_dfa(make_single_dfa("RPAREN", ")"))
     token_scanner.add_dfa(make_single_dfa("LCURBRACKET", "{"))
     token_scanner.add_dfa(make_single_dfa("RCURBRACKET", "}"))
-    token_scanner.add_dfa(make_single_dfa("EQUAL", "="))
-    token_scanner.add_dfa(make_single_dfa("SEMICOLON", ";"))
 
-    iden_dfa = Dfa("identifier")
-    iden_dfa.set_final_states([1])
-    iden_dfa.add_rule(0, 1, char)
-    iden_dfa.add_rule(1, 1, digit)
-    iden_dfa.add_rule(1, 1, char)
-    token_scanner.add_dfa(iden_dfa)
+    # comma
+    token_scanner.add_dfa(make_single_dfa("COMMA", ","))
 
-    iden_dfa = Dfa("digits")
-    iden_dfa.set_final_states([1])
-    iden_dfa.add_rule(0, 1, digit)
-    iden_dfa.add_rule(1, 1, digit)
-    iden_dfa.add_rule(0, 2, "-")
-    iden_dfa.add_rule(2, 1, digit)
-    iden_dfa.add_rule(1, 3, ".")
-    token_scanner.add_dfa(iden_dfa)
+    # integer
+    integer_dfa = Dfa("Integer")
+    integer_dfa.set_final_states([1, 3])
+    integer_dfa.add_rule(0, 1, "0")
+    integer_dfa.add_rule(0, 2, "-")
+    integer_dfa.add_rule(0, 3, nz)
+    integer_dfa.add_rule(2, 3, nz)
+    integer_dfa.add_rule(3, 3, digit)
+    token_scanner.add_dfa(integer_dfa)
 
-    iden_dfa = Dfa("float")
-    iden_dfa.set_final_states([4])
-    iden_dfa.add_rule(0, 1, "-")
-    iden_dfa.add_rule(1, 2, digit)
-    iden_dfa.add_rule(0, 2, digit)
-    iden_dfa.add_rule(2, 3, ".")
-    iden_dfa.add_rule(3, 4, digit)
-    token_scanner.add_dfa(iden_dfa)
+    # literal
+    literal_dfa = Dfa("Literal")
+    literal_dfa.set_final_states([2])
+    literal_dfa.add_rule(0, 1, "\"")
+    literal_dfa.add_rule(1, 1, digit)
+    literal_dfa.add_rule(1, 1, char)
+    literal_dfa.add_rule(1, 1, " ")
+    literal_dfa.add_rule(1, 2, "\"")
+    token_scanner.add_dfa(literal_dfa)
 
-    blank = "\n\r "
-    blank_dfa = Dfa("blank")
-    blank_dfa.set_final_states([1])
-    blank_dfa.add_rule(0, 1, blank)
-    blank_dfa.add_rule(1, 1, blank)
-    token_scanner.add_dfa(blank_dfa)
+    # boolean
+    bool_dfa = Dfa("Boolean")
+    bool_dfa.set_final_states([4, 9])
+    literal_dfa.add_rule(0, 1, "t")
+    literal_dfa.add_rule(1, 2, "r")
+    literal_dfa.add_rule(2, 3, "u")
+    literal_dfa.add_rule(3, 4, "e")
 
-    quote = '"'
-    blank_dfa = Dfa("quote")
-    blank_dfa.set_final_states([2])
-    blank_dfa.add_rule(0, 1, quote)
-    blank_dfa.add_rule(1, 1, digit + char)
-    blank_dfa.add_rule(1, 2, quote)
-    token_scanner.add_dfa(blank_dfa)
+    literal_dfa.add_rule(0, 5, "f")
+    literal_dfa.add_rule(5, 6, "a")
+    literal_dfa.add_rule(6, 7, "l")
+    literal_dfa.add_rule(7, 8, "s")
+    literal_dfa.add_rule(8, 9, "e")
+    token_scanner.add_dfa(literal_dfa)
+
+    #float
+    float_dfa = Dfa("float")
+    float_dfa.set_final_states([5])
+    float_dfa.add_rule(0, 1, "-")
+    float_dfa.add_rule(0, 2, nz)
+    float_dfa.add_rule(0, 3, "0")
+    float_dfa.add_rule(1, 2, nz)
+    float_dfa.add_rule(1, 3, "0")
+    float_dfa.add_rule(2, 2, digit)
+    float_dfa.add_rule(2, 4, ".")
+    float_dfa.add_rule(3, 4, ".")
+    float_dfa.add_rule(4, 5, digit)
+    float_dfa.add_rule(5, 5, nz)
+    float_dfa.add_rule(5, 6, "0")
+    float_dfa.add_rule(6, 5, nz)
+    float_dfa.add_rule(6, 6, "0")
+    token_scanner.add_dfa(float_dfa)
+
+    #id
+    id_dfa = Dfa("ID")
+    id_dfa.set_final_states([1])
+    id_dfa.add_rule(0, 1, char)
+    id_dfa.add_rule(0, 1, "_")
+    id_dfa.add_rule(1, 1, char)
+    id_dfa.add_rule(1, 1, "_")
+    id_dfa.add_rule(1, 1, digit)
+    token_scanner.add_dfa(id_dfa)
 
 def main(file_path):
     with open(file_path, mode="r") as f:
