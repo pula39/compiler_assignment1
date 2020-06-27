e22d847f1dbab709d598f65867f3771a4cdb6f36
sy10kr <alswnsghd15@naver.com>
2020-06-27 16:09:33 +0900

    nfa, dfa change

---
+++ test_1.c
@@ -0,0 +1,32 @@
+
+int b = (100/30);
+int c = z;
+int d = 100 + 100;
+int e = 100 - 100;
+int f = 100 * 100;
+int g = 100*(100*100);
+
+int sstr = "aa";
+char ssstr = "aaa";
+
+float main(float r, float b){
+
+    int a = 100/30;
+
+    if( a > b){
+        a = 30;
+    }
+    else{
+        a = 20;
+    }
+
+    while( a > b){
+        a = 30;
+    }
+
+    for(i = 0 ; (i+3) < 30; i = i + 1){
+        i = i*2;
+    }
+
+    return r;
+}
